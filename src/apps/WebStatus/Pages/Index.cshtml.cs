42de78d8c28352f933248b6811893e58dd4d01e3
Simon McKenna <sshnug.si+github@gmail.com>
2022-03-13 15:46:27 +1030

    Fix namespaces

--- src/apps/WebStatus/Pages/Index.cshtml.cs
+++ src/apps/WebStatus/Pages/Index.cshtml.cs
@@ -1,20 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+namespace WebStatus.Pages;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebStatus.Pages
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
     {
-        private readonly ILogger<IndexModel> _logger;
+        _logger = logger;
+    }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+    public void OnGet()
+    {
 
-        public void OnGet()
-        {
-
-        }
     }
 }
